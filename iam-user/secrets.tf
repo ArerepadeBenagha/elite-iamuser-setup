0d8d8bd70ae212d3b901befeb93bff580ef69575
ArerepadeB <aredelulu@gmail.com>
2021-08-09 00:02:28 -0500

    remove duplicate secrets

--- iam-user/secrets.tf
+++ iam-user/secrets.tf
@@ -1,8 +1,3 @@
-resource "aws_ssm_parameter" "eliteadminuser_ssm" {
-  name  = join("-", [local.application.app_name, "eliteadminuser_ssm"])
-  type  = "String"
-  value = "6KBK4pH8W7x1W3wN"
-}
 resource "aws_ssm_parameter" "secret" {
   name        = join("-", [local.application.app_name, "eliteadminuser_ssmsecrets"])
   description = "The parameter description"
