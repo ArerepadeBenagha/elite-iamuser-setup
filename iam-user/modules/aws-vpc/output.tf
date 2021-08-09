4b264777e3647cb41e527c98d1fd7135cbf411a6
ArerepadeB <aredelulu@gmail.com>
2021-08-08 23:55:59 -0500

    setup module for vpc

---
+++ iam-user/modules/aws-vpc/output.tf
@@ -0,0 +1,18 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = concat(aws_vpc.main.*.id, [""])[0]
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = concat(aws_vpc.main.*.arn, [""])[0]
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = concat(aws_vpc.main.*.cidr_block, [""])[0]
+}
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = concat(aws_vpc.main.*.default_security_group_id, [""])[0]
+}
--- iam-user/outputs.tf
+++ iam-user/outputs.tf
@@ -1,6 +1,6 @@
-# output "password" {
-#   value = aws_iam_user_login_profile.eliteuseradmin_login.encrypted_password
-# }
+output "password" {
+  value = aws_iam_user_login_profile.eliteuseradmin_login.encrypted_password
+}
 output "user" {
   value = aws_iam_user.eliteuseradmin.name
 }
